66c870fc4790b58ae2094d1504994b846cb58ce3
httpc <makshttpc@gmail.com>
2020-05-15 21:07:35 +0300

    re

--- Exam/Source.cpp
+++ Exam/Source.cpp
@@ -2,28 +2,25 @@
 #include <time.h>       
 #include <stdlib.h>
 #include <string.h>
-
+#include <windows.h>
+#include<conio.h>
 using namespace std;
+
+void Random(char* brr, int n)
+{
+    for (int i = 0; i < n; i++) {
+        i % 8 == 0 && i != 0 ? *(brr+i) = char(32) : *(brr+i) = char(rand() % 25 + 97); 
+    }
+}
 
 int main() {
     srand(time(0));
-    const int N = 10;           
+    const int N = 24;           
     const int MAX_LEN = 255;   
     
-
-    
-    const char* const DICTIONARY_OF_WORDS[N] = {
-        "hello",
-        "mother",
-        "father",
-        "world",
-        "car",
-        "little",
-        "magazine",
-        "journal",
-        "cake",
-        "compilator"
-    };
+    char* DICTIONARY_OF_WORDS= new char[N];
+    int cin[N];
+      
 
     int random;                                  
     int score = 0;                               
@@ -33,15 +30,27 @@ int main() {
     clock_t start = clock();                         
 
     while (clock() - start < delay) {
-        random = rand() % N;                        
-        cout  << DICTIONARY_OF_WORDS[random] << '\n';
-        cin.getline(S, MAX_LEN);
-        if (strcmp(S, DICTIONARY_OF_WORDS[random]) == 0) {
-            score++; 
-        }
-    }
+        Random(DICTIONARY_OF_WORDS, N);
+        for (int i = 0; i < N; ++i)
+        {
 
+        cout  << DICTIONARY_OF_WORDS[i];
+        }
+        cout << endl;
+        for (int i = 0; i < N; ++i)
+        {
+            cin[i] = _getch();
+           
+                cout << char(cin[i]);
+           
+            if (cin[i] == DICTIONARY_OF_WORDS[i])
+            {
+                score++;
+            }
+            
+        }
+        cout << endl;
+        system("CLS");
+    }
     cout << "Score = " << score << '\n';
-    cout << "count words == " << N;
-    
 }
